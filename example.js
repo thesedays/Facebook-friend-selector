8c4e1e0674f4e722e4159afb52d5287c26c838fa
Keegan Street <keeganstreet@gmail.com>
2011-06-07 21:45:47 +0200

    added some info to the example page

--- example.js
+++ example.js
@@ -6,45 +6,43 @@ window.fbAsyncInit = function () {
 	FB.init({appId: '172102396182433', status: true, cookie: false, xfbml: false});
 
 	$(document).ready(function () {
-		var selector1, selector2, callbackFriendSelected, callbackFriendUnselected, callbackMaxSelection, callbackSubmit;
+		var selector1, selector2, logActivity, callbackFriendSelected, callbackFriendUnselected, callbackMaxSelection, callbackSubmit;
 
 		TDFriendSelector.init({debug: true});
 
 		selector1 = TDFriendSelector.newInstance();
 		selector2 = TDFriendSelector.newInstance({maxSelection: 2, friendsPerPage: 5});
 
+		logActivity = function (message) {
+			$("#results").append('<div>' + new Date() + ' - ' + message + '</div>');
+		};
+
 		callbackFriendSelected = function(friendId) {
-			var result, friend, name, currentDate = new Date();
+			var friend, name;
 			friend = TDFriendSelector.getFriendById(friendId);
 			name = friend.name;
-			result = $('<div>' + currentDate + ' - Selected ' + name + ' (ID: ' + friendId + ')</div>');
-			$("#results").append(result);
+			logActivity('Selected ' + name + ' (ID: ' + friendId + ')');
 		};
 		selector1.setCallbackFriendSelected(callbackFriendSelected);
 		selector2.setCallbackFriendSelected(callbackFriendSelected);
 
 		callbackFriendUnselected = function(friendId) {
-			var result, friend, name, currentDate = new Date();
+			var friend, name;
 			friend = TDFriendSelector.getFriendById(friendId);
 			name = friend.name;
-			result = $('<div>' + currentDate + ' - Unselected ' + name + ' (ID: ' + friendId + ')</div>');
-			$("#results").append(result);
+			logActivity('Unselected ' + name + ' (ID: ' + friendId + ')');
 		};
 		selector1.setCallbackFriendUnselected(callbackFriendUnselected);
 		selector2.setCallbackFriendUnselected(callbackFriendUnselected);
 
 		callbackMaxSelection = function() {
-			var result, currentDate = new Date();
-			result = $('<div>' + currentDate + ' - Selected the maximum number of friends</div>');
-			$("#results").append(result);
+			logActivity('Selected the maximum number of friends');
 		};
 		selector1.setCallbackMaxSelection(callbackMaxSelection);
 		selector2.setCallbackMaxSelection(callbackMaxSelection);
 
 		callbackSubmit = function(selectedFriendIds) {
-			var result, currentDate = new Date();
-			result = $('<div>' + currentDate + ' - Clicked OK with the following friends selected: ' + selectedFriendIds.join(", ") + '</div>');
-			$("#results").append(result);
+			logActivity('Clicked OK with the following friends selected: ' + selectedFriendIds.join(", "));
 		};
 		selector1.setCallbackSubmit(callbackSubmit);
 		selector2.setCallbackSubmit(callbackSubmit);
